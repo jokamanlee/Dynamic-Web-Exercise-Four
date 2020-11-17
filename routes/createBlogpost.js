aabb4c5cbc45210b6d553175b5998667bf7e8287
jokamanlee <jl10630@nyu.edu>
2020-11-17 14:22:15 -0500

    Add create post

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -10,7 +10,7 @@ const db = firebase.firestore();
 const blogposts = db.collection("blogposts");
 
 const form = `
-    <form>
+    <form action="/create/submit">
         <input type="text" name="title" placeholder="Title of Post"/>
         <input type="text" name="text" placeholder="Text of Post"/>
         <input type="text" name="author" placeholder="Author"/>
@@ -19,5 +19,22 @@ const form = `
 `;
 
 router.get("/", (req, res) => res.send(form));
+// Route for submitting the form....
+router.get("/submit", (req, res) => {
+  const queryParams = req.query; // ?title=words&text=words&author=words
+
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
+
+  blogposts
+    .doc(idFromTitle)
+    .set(queryParams)
+    .then(function (doc) {
+      res.send("Successful Submission");
+    })
+    .catch(function (error) {
+      console.log("error", error);
+      res.send("Failed Submission");
+    });
+});
 
 module.exports = router;
